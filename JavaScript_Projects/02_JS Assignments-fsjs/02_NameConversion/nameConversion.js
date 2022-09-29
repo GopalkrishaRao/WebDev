994011568077904d1e5a235ca5c4218eb155b834
GK Rao <hrgkrao@gmail.com>
2022-09-29 19:36:39 +0530

    js

---
+++ JavaScript_Projects/02_JS Assignments-fsjs/02_NameConversion/nameConversion.js
@@ -0,0 +1,22 @@
+// let a =[];
+// document.getElementById("convert-btn").onclick = converter;
+// let inputText = document.getElementById("text");
+
+// function converter(){
+//     a.push(inputText.value)
+//     console.log(a);
+
+
+    
+// }
+let a =[];
+document.getElementById("convert-btn").onclick = converter;
+let inputText = document.getElementById("text");
+
+function converter(){
+    a.push(inputText.value)
+    console.log(a[0].toUpperCase());
+
+    
+    
+}

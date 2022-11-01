43033cc831bd6ac32a0f757833af0d80c9ff64c4
GK Rao <hrgkrao@gmail.com>
2022-11-01 21:26:37 +0530

    js project-passwordGenerator

--- JavaScript_Projects/02_JS Assignments-fsjs/04_PasswordGenerator/script.js
+++ JavaScript_Projects/02_JS Assignments-fsjs/04_PasswordGenerator/script.js
@@ -1,53 +1,61 @@
-const resultEl = document.getElementById('result')
-const lengthEl = document.getElementById('length')
-const uppercaseEl = document.getElementById('uppercase')
-const lowercaseEl = document.getElementById('lowercase')
-const numbersEl = document.getElementById('numbers')
-const symbolsEl = document.getElementById('symbols')
-const generateEl = document.getElementById('generate')
-const clipboardEl = document.getElementById('clipboard') 
+// const resultEl = document.getElementById('result');
+// const lengthEl = document.getElementById('length');
+// const uppercaseEl = document.getElementById('uppercase');
+// const lowercaseEl = document.getElementById('lowercase');
+// const numbersEl = document.getElementById('numbers');
+// const symbolsEl = document.getElementById('symbols');
+// const generateEl = document.getElementById('generate');
+// const clipboardEl = document.getElementById('clipboard') ;
 
-let lengthofPassword = document.getElementById('length').value;
+// let passwordLength = document.getElementById('length').value;
 
 
+let passwordType=[];
 
-const randomFunc = {
-    lower: getRandomLower,
-    upper: getRandomUpper,
-    number: getRandomNumber,
-    symbol: getRandomSymbol
-}
+let password = [];
 
-clipboardEl.addEventListener('click', () => {
-    
-})
-
-generateEl.addEventListener('click', () => {
-    
-})
-
-function generatePassword(lower, upper, number, symbol, length) {
-    
-}
-
-let alphabet = "abcdefghijklmnopqrstuvwxyz"
-function getRandomLower() {
-    return alphabet[Math.floor(Math.random()*alphabet.length)];
-}
-
-function getRandomUpper() {
-    let x = alphabet[Math.floor(Math.random()*alphabet.length)];
-    return x.toUpperCase();
-}
-
-function getRandomNumber() {
+let num = ()=>{
     return Math.floor(Math.random()*10)
-}
+};
 
-let symbols = "!@#$%&*_+-=<>?"
-function getRandomSymbol() {
+let symbol = ()=>{
+    let symbols = "!@#$%^&*_-+=/"
     return symbols[Math.floor(Math.random()*symbols.length)]
-}
+};
+
+let upper = ()=>{
+    let alphabet = "abcdefghijklmnopqrstuvwxyz"
+    return alphabet[Math.floor(Math.random()*alphabet.length)].toUpperCase()
+};
+
+let lower = ()=>{return upper().toLocaleLowerCase()};
 
 
-document.body.style.color="#1f1f1f"
+
+
+
+
+
+
+
+// const randomFunc = {
+//     lower: getRandomLower,
+//     upper: getRandomUpper,
+//     number: getRandomNumber,
+//     symbol: getRandomSymbol
+// }
+
+// clipboardEl.addEventListener('click', () => {
+    
+// })
+
+// generateEl.addEventListener('click', () => {
+    
+// })
+
+// function generatePassword(lower, upper, number, symbol, length) {
+    
+// }
+
+
+// document.body.style.color="#1f1f1f"

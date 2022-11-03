665777b6b6fb9d1bac9883b1319a88b2897a5464
GK Rao <hrgkrao@gmail.com>
2022-11-03 17:11:54 +0530

    jsproject-Password generator

--- JavaScript_Projects/02_JS Assignments-fsjs/04_PasswordGenerator/script.js
+++ JavaScript_Projects/02_JS Assignments-fsjs/04_PasswordGenerator/script.js
@@ -1,58 +1,77 @@
-// const resultEl = document.getElementById('result');
+const resultEl = document.getElementById('result');
 // const lengthEl = document.getElementById('length');
-// const uppercaseEl = document.getElementById('uppercase');
-// const lowercaseEl = document.getElementById('lowercase');
-// const numbersEl = document.getElementById('numbers');
-// const symbolsEl = document.getElementById('symbols');
-// const generateEl = document.getElementById('generate');
+const uppercaseEl = document.getElementById('uppercase');
+const lowercaseEl = document.getElementById('lowercase');
+const numbersEl = document.getElementById('numbers');
+const symbolsEl = document.getElementById('symbols');
+const generateEl = document.getElementById('generate');
 // const clipboardEl = document.getElementById('clipboard') ;
 
-let passwordType=[];
-let password = []; 
+generateEl.addEventListener("click", gneratePassword)
 
-let passwordLength = document.getElementById('length').value;
+function gneratePassword(){
+    // chech what charects must include in password
+    let passwordIncluedes=[];
 
-for(i=0; i<passwordLength; i++){
-   console.log( Math.floor(Math.random()*passwordType.length));
-};
+    if(uppercaseEl.checked==true){
+        passwordIncluedes.push("upperCase")
+    };
+    if(lowercaseEl.checked==true){
+        passwordIncluedes.push("lowerCase")
+    };
+    if(numbersEl.checked==true){
+        passwordIncluedes.push("number")
+    };
+    if(symbolsEl.checked==true){
+        passwordIncluedes.push("symbols")
+    };
 
-
-
-let num = ()=>{
-    return Math.floor(Math.random()*10)
-};
-
-let symbol = ()=>{
-    let symbols = "!@#$%^&*_-+=/"
-    return symbols[Math.floor(Math.random()*symbols.length)]
-};
-
-let upper = ()=>{
-    let alphabet = "abcdefghijklmnopqrstuvwxyz"
-    return alphabet[Math.floor(Math.random()*alphabet.length)].toUpperCase()
-};
-
-let lower = ()=>{return upper().toLocaleLowerCase()};
-
-
-// const randomFunc = {
-//     lower: getRandomLower,
-//     upper: getRandomUpper,
-//     number: getRandomNumber,
-//     symbol: getRandomSymbol
-// }
-
-// clipboardEl.addEventListener('click', () => {
+    // required length of password
+    let passwordLength = document.getElementById('length').value;
     
-// })
+    let random = []
+    for(i=0; i<passwordLength; i++){
+    random.push(Math.floor(Math.random()*passwordIncluedes.length));
+    };
 
-// generateEl.addEventListener('click', () => {
-    
-// })
+    // functions to generate number, upper and lowercase and symbols
 
-// function generatePassword(lower, upper, number, symbol, length) {
-    
-// }
+    let num = ()=>{
+        return Math.floor(Math.random()*10)
+    };
+
+    let symbol = ()=>{
+        let symbols = "!@#$%^&*_-+=/"
+        return symbols[Math.floor(Math.random()*symbols.length)]
+    };
+
+    let upper = ()=>{
+        let alphabet = "abcdefghijklmnopqrstuvwxyz"
+        return alphabet[Math.floor(Math.random()*alphabet.length)].toUpperCase()
+    };
+
+    let lower = ()=>{return upper().toLocaleLowerCase()};
 
 
-// document.body.style.color="#1f1f1f"
+    // Password generation
+    let password = []
+    for(i=0; i<random.length; i++){
+        if(random[i]==0){
+            password.push(upper())
+        }
+        else if (random[i]==1){
+            password.push(lower())
+        }
+        else if (random[i]==2){
+            password.push(num())
+        }
+        else{
+            password.push(symbol())  
+        }
+    };
+    resultEl.innerText=password.join("")
+}
+
+
+
+document.body.style.color="#1f1f1f"

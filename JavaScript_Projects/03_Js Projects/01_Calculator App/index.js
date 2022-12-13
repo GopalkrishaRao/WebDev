153e9a246e7c763c66333d454503ddab0d82edab
GK Rao <hrgkrao@gmail.com>
2022-12-13 22:25:37 +0530

    calculator some functions in js

--- JavaScript_Projects/03_Js Projects/01_Calculator App/index.js
+++ JavaScript_Projects/03_Js Projects/01_Calculator App/index.js
@@ -1,2 +1,11 @@
 let display= document.getElementById('display');
 let buttons = document.getElementsByClassName('button');
+
+buttons.map(button => {
+    button.addEventListner('click', (e)=>{
+        console.log('clicked');
+        console.log(e);
+        console.log(e.target);
+        console.log(e.target.innerText);
+    });
+});

41694cb649e7bef7430d21d37028c3ec1553119b
GK Rao <hrgkrao@gmail.com>
2022-12-16 21:35:35 +0530

    Error handling changes

--- JavaScript_Projects/03_Js Projects/01_Calculator App/index.js
+++ JavaScript_Projects/03_Js Projects/01_Calculator App/index.js
@@ -2,28 +2,39 @@ let display= document.getElementById('display');
 let buttons =Array.from(document.getElementsByClassName('button'))
 
 
+
 buttons.map(button => {
     button.addEventListener('click', (e)=>{
-        switch(e.target.innerText){
-            case 'C': 
-                display.innerText ='';
-                break;
-            case '←':
-                if(display.innerText){
-                    display.innerText = display.innerText.slice(0,-1);
+
+         let cal=()=>{
+            switch(e.target.innerText){
+                case 'C': 
+                    display.innerText ='';
+                    break;
+                case '←':
+                    if(display.innerText){
+                        display.innerText = display.innerText.slice(0,-1);
+                    }
+                    break;
+                case '=':
+                    try{
+                        display.innerText = eval(display.innerText);
+                    }
+                    catch{
+                        display.innerText="Error"
+                    }
+                    break;
+                default: 
+                    display.innerText += e.target.innerText;
                 }
-                break;
-            case '=':
-                try{
-                    display.innerText = eval(display.innerText);
-                }
-                catch{
-                    display.innerText="Error"
-                }
-                break;
-            default: 
-                display.innerText += e.target.innerText;
-             
         }
+        if(display.innerText !='Error'){
+            cal();
+        }
+        else{
+            display.innerText= '';
+            cal();
+        }
+        
     });
 });

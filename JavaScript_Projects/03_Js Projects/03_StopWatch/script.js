491d2efca2edcc0fc04df77a5d48e738234479cc
GK Rao <hrgkrao@gmail.com>
2022-12-19 19:03:47 +0530

    js calculator final

--- JavaScript_Projects/03_Js Projects/03_StopWatch/script.js
+++ JavaScript_Projects/03_Js Projects/03_StopWatch/script.js
@@ -1,43 +1,46 @@
-let seconds =00;
-let tens = 00;
+let min =00;
+let seconds = 00;
 
+
+let getMin = document.querySelector('.min');
 let getSeconds = document.querySelector('.seconds');
-let getTens = document.querySelector('.tens');
 let btnStart  = document.querySelector('.btn-start');
-let btnStop  = document.querySelector('.btn-stop');
 let btnReset  = document.querySelector('.btn-reset');
-let interval;
+
+let interval
+btnStart.addEventListener('click', ()=>{
+    if(btnStart.innerHTML=='Start' || btnStart.innerHTML=='Resume'){
+        interval = setInterval(startTimer, 1000)
+        btnStart.innerHTML='Puase'
+    }
+    else{
+        clearInterval(interval);
+        btnStart.innerHTML='Resume'
+    }
+ })
+
 function startTimer(){
-    tens++;
-    if(tens<=9){
-        getTens.innerHTML='0'+ tens;
-    }
-    if(tens >9){
-        getTens.innerHTML=+ tens;
-    }
-    if(tens > 99){
-        seconds++;
+    seconds++;
+    if(seconds<=9){
         getSeconds.innerHTML='0'+ seconds;
-        tens=0;
-        getTens.innerHTML='0'+ 0;
     }
-    if(seconds >9){
+    else if(seconds >9 && seconds<60){
         getSeconds.innerHTML= seconds;
+    }
+    else if(seconds == 60){
+        min= min+1;
+        getMin.innerHTML='0'+ min;
+        seconds=0;
+        getSeconds.innerHTML='0'+ 0;
     }
 }
 
-btnStart.addEventListener('click', ()=>{
-   interval =  setInterval(startTimer, 10)
-})
-
-btnStop.addEventListener('click', ()=>{
- clearInterval(interval)
-})
 
 btnReset.addEventListener('click', ()=>{
     clearInterval(interval);
-    tens = '00';
     seconds = '00';
+    min = '00';
+    getMin.innerHTML=min;
     getSeconds.innerHTML=seconds;
-    getTens.innerHTML=tens;
+    btnStart.innerHTML='Start'
    })

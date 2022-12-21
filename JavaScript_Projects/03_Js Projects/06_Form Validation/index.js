00073ab043da33dcd7144fa9ce1d636c8710ad73
GK Rao <hrgkrao@gmail.com>
2022-12-21 20:15:12 +0530

    Form validation

--- JavaScript_Projects/03_Js Projects/06_Form Validation/index.js
+++ JavaScript_Projects/03_Js Projects/06_Form Validation/index.js
@@ -1,8 +1,42 @@
 let signUp = document.querySelector('#signup');
+let checkBox = document.querySelector('.check');
+let viewPassword = document.querySelectorAll('.viewpw')[0];
+let password = document.querySelector('.pass_word');
+let confirmPassword = document.querySelector('.con_pass');
+let form = document.querySelector('.inputarea');
 
+form.addEventListener("submit", (e)=>{
+    e.preventDefault()
+})
 
-let validate = ()=>{
-    console.log("validated");
+let validateForm = ()=>{
+    
+  
+    if(password.value!=confirmPassword.value){
+        confirmPassword.style.border= "2px solid red";
+        alert ("Password does not match");
+    }
+    else{
+        password.style.border= "2px solid green";
+        confirmPassword.style.border= "2px solid green";
+        setTimeout(()=>{
+            alert("Form has been submitted")
+            form.submit()
+        }, 2000)
+    }
 };
 
-signUp.addEventListener('click', validate);
+signUp.addEventListener('click', validateForm);
+
+// check box password visibility
+checkBox.addEventListener('click', ()=>{
+    if(viewPassword.type==='password'){
+        viewPassword.type = 'text';
+        confirmPassword.type='text';
+    }
+    else{
+        viewPassword.type='password';
+        confirmPassword.type='password';
+    }
+});
+

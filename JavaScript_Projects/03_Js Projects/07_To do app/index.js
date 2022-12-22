f9f7964e3616a1a3c98ee2958f140ce391eaa696
GK Rao <hrgkrao@gmail.com>
2022-12-22 22:10:17 +0530

    Add to do list feature

--- JavaScript_Projects/03_Js Projects/07_To do app/index.js
+++ JavaScript_Projects/03_Js Projects/07_To do app/index.js
@@ -3,8 +3,34 @@ let task = document.getElementsByClassName('todo_details')[0];
 let starBtn = document.getElementsByClassName('star')[0];
 let editBtn = document.getElementsByClassName('edit')[0];
 let deleteBtn = document.getElementsByClassName('delete')[0];
+let addBtn = document.getElementsByClassName('add')[0];
+let addToDo = document.getElementsByClassName('myInput')[0];
+let newChild 
 
-
+let addTask = () =>{
+    let createOl =document.createElement('ol');
+    createOl.innerHTML=
+    `
+    <div class="todo_body">
+        <input type="checkbox" name="check" id="task_01" class="checkbox" onclick="strike()">
+         <h3 class="todo_details">
+            ${addToDo.value}
+        </h3>
+    </div>
+    <div class="editables">
+        <button class="star" onclick="starTask()">
+            Star
+        </button>
+        <button class="edit" onclick="editTask()">
+            Edit
+        </button>
+        <button class="delete" onclick="deleteTask()">
+            Delete
+        </button>
+    </div> `
+    document.getElementsByClassName('container')[0].appendChild(createOl);
+    addToDo.value=''
+}
 
 let strike = ()=>{
     if(check.checked){
@@ -28,10 +54,10 @@ let deleteTask = ()=>{
 
 let editTask = ()=>{
     if(editBtn.innerHTML=='Edit'){
-        editBtn.innerHTML='Save'
+        editBtn.innerHTML='Save';
+        // task.innerHTML=`<input type='text'>`
     }
     else {
         editBtn.innerHTML='Edit'
     }
-    console.log('edit task');
 }

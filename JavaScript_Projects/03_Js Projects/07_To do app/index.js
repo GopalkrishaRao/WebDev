5d6dc847daaca6b9fb812735a897554b601e292b
GK Rao <hrgkrao@gmail.com>
2023-01-05 13:35:31 +0530

    To do app update with table format

--- JavaScript_Projects/03_Js Projects/07_To do app/index.js
+++ JavaScript_Projects/03_Js Projects/07_To do app/index.js
@@ -1,63 +1,55 @@
-let check = document.getElementsByClassName('checkbox')[0];
-let task = document.getElementsByClassName('todo_details')[0];
-let starBtn = document.getElementsByClassName('star')[0];
-let editBtn = document.getElementsByClassName('edit')[0];
-let deleteBtn = document.getElementsByClassName('delete')[0];
 let addBtn = document.getElementsByClassName('add')[0];
 let addToDo = document.getElementsByClassName('myInput')[0];
-let newChild 
+let editBtn = document.getElementsByClassName('edit-btn')[0];
+let deleteBtn = document.getElementsByClassName('delete-btn')[0];
+let editBtnDiv = document.getElementsByClassName('editbtndiv')[0];
+let chekBox = document.getElementsByClassName('check-btn')[0];
+let taskDetails = document.getElementsByClassName('task')[0];
+let newChild ;
+
+let edit = ()=>{
+            if(editBtn.innerHTML=='Edit'){
+                editBtn.innerHTML='Save';
+                taskDetails.contentEditable = true;
+            }
+            else{
+                editBtn.innerHTML='Edit';
+                taskDetails.contentEditable = false;
+            }
+        };
+
+        let delt = ()=>{
+        };
+
+        function taskcheck(){
+            if(chekBox.checked == true){
+                taskDetails.innerHTML=`<s>${taskDetails.innerHTML}</s>`;
+
+                editBtnDiv.style.display='none'
+            }
+            else{
+                taskDetails.innerHTML=taskDetails.innerText;
+                editBtnDiv.style.display='block'
+            }
+        };
 
 let addTask = () =>{
-    let createOl =document.createElement('ol');
+    let createOl =document.createElement('tr');
     createOl.innerHTML=
     `
-    <div class="todo_body">
-        <input type="checkbox" name="check" id="task_01" class="checkbox" onclick="strike()">
-         <h3 class="todo_details">
-            ${addToDo.value}
-        </h3>
-    </div>
-    <div class="editables">
-        <button class="star" onclick="starTask()">
-            Star
-        </button>
-        <button class="edit" onclick="editTask()">
-            Edit
-        </button>
-        <button class="delete" onclick="deleteTask()">
-            Delete
-        </button>
-    </div> `
+    <td>
+                <input class="check-btn" type="checkbox" onclick="taskcheck()">
+                </td>
+                <td style="width: 80%;" contenteditable="false" class="task">
+                    <h4>${addToDo.value}</h4>  
+                </td>
+                <td class="editbtndiv">
+                    <button class="edit-btn" onclick="edit()">Edit</button>
+                </td>
+                <td>
+                    <button class="delete-btn" onclick="delt()">Delete</button>
+                </td>
+    `
     document.getElementsByClassName('container')[0].appendChild(createOl);
     addToDo.value=''
-}
-
-let strike = ()=>{
-    if(check.checked){
-        task.innerHTML=`<s>${task.innerHTML}</s>`
-    }
-    // else if (!check.checked){
-        //     task.innerHTML=task.innerText
-        // }
-    else {
-         task.innerHTML=task.innerText
-        }
-}
-
-let starTask = ()=>{
-    console.log("importent");
-}
-
-let deleteTask = ()=>{
-    console.log("delete");
-}
-
-let editTask = ()=>{
-    if(editBtn.innerHTML=='Edit'){
-        editBtn.innerHTML='Save';
-        // task.innerHTML=`<input type='text'>`
-    }
-    else {
-        editBtn.innerHTML='Edit'
-    }
 }

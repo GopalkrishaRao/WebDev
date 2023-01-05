9bc2dd36a02a18bb706d03a16a5b5f1756f4776f
GK Rao <hrgkrao@gmail.com>
2023-01-05 16:51:38 +0530

    delete function added to do

--- JavaScript_Projects/03_Js Projects/07_To do app/index.js
+++ JavaScript_Projects/03_Js Projects/07_To do app/index.js
@@ -1,4 +1,7 @@
 let addBtn = document.getElementsByClassName('add')[0];
+let tableRow = document.getElementsByClassName('tablerows')[0];
+let table = document.getElementsByClassName('table')[0];
+
 let addToDo = document.getElementsByClassName('myInput')[0];
 let editBtn = document.getElementsByClassName('edit-btn')[0];
 let deleteBtn = document.getElementsByClassName('delete-btn')[0];
@@ -7,6 +10,7 @@ let chekBox = document.getElementsByClassName('check-btn')[0];
 let taskDetails = document.getElementsByClassName('task')[0];
 let newChild ;
 
+// Edit to do list
 let edit = ()=>{
             if(editBtn.innerHTML=='Edit'){
                 editBtn.innerHTML='Save';
@@ -18,16 +22,19 @@ let edit = ()=>{
             }
         };
 
+// Deleting a task
         let delt = ()=>{
+            if(confirm('This task will be deleted')){
+                table.removeChild(table.firstElementChild)
+            }
         };
-
+// Check box to strick out the task that are completed
         function taskcheck(){
             if(chekBox.checked == true){
                 taskDetails.innerHTML=`<s>${taskDetails.innerHTML}</s>`;
                 editBtn.innerHTML='Edit';
                 taskDetails.contentEditable = false;
                 editBtnDiv.style.display='none'
-                // taskDetails.colSpan ='2'
             }
             else{
                 taskDetails.innerHTML=taskDetails.innerText;
@@ -35,6 +42,7 @@ let edit = ()=>{
             }
         };
 
+// Add task  to do list
 let addTask = () =>{
     let createOl =document.createElement('tr');
     createOl.setAttribute('class','tablerows' )
@@ -53,8 +61,6 @@ let addTask = () =>{
                     <button class="delete-btn" onclick="delt()">Delete</button>
                 </td>
     `
-
     document.getElementsByClassName('table')[0].appendChild(createOl);
-    
     addToDo.value=''
 };
